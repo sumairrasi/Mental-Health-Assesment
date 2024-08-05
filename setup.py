fefbc6531601df339e87896fb743f5788d8a87a0
sumairrazi585@gmail.com <sumairrazi585@gmail.com>
2024-08-06 00:09:46 +0530

    requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Mental Health',
+    version='0.0.0',
+    author="Sumair Rasi",
+    author_email="sumairrazi585@gmail.com",
+    packages=find_packages()
+)
